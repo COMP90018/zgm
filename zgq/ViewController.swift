aa8fe8ed9706033c139963d96d60f67aadb943ab
Arthur <donngao@gmail.com>
2017-09-09 14:10:44 +1000

    Initial Commit

---
+++ zgq/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zgq
+//
+//  Created by DongGao on 9/9/17.
+//  Copyright © 2017 TheGreatMind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
